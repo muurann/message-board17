37b04b7b4967c5254c1d9d50d5edcc11c495af91
Issei.Muratani <muurann@gmail.com>
2017-01-06 09:52:37 +0000

    varidation 一部体裁崩れ

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,8 @@
 class Message < ActiveRecord::Base
-
+    #　名前は必須かつ２０文字以内
+    validates :name ,length:{ maximum:20},presence:true
+    
+    #　内容は必須かつ２文字以上30文字以内
+    validates :body ,length:{ minimum:2,maximum:30},presence:true
+    
 end
